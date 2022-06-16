1e258fbc7d6221ca2cc820ec9af839f8d25c95e6
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-16 12:51:04 +0530

    Added a complex reducer logic to only increase the amount if the same item is added to the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -6,9 +6,30 @@ const defaultCartState = {
 };
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItem;
+    let updatedItems;
+
+    if (existingCartItem) {
+      updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+      console.log(existingCartItem);
+    } else {
+      updatedItem = { ...action.item };
+      updatedItems = state.items.concat(updatedItem);
+      console.log("new item");
+    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
