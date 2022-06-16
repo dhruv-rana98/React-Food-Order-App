51a77bd37861f1705adc3a1a504763ad8842a987
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-16 12:50:15 +0530

    Issue fixed for undefined id which was causing only a single item to render even after updating the states

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,7 +32,7 @@ const DUMMY_MEALS = [
 const AvailableMeals = (props) => {
   const mealList = DUMMY_MEALS.map((meal) => (
     <MealItem
-      id={props.id}
+      id={meal.id}
       key={meal.id}
       name={meal.name}
       description={meal.description}
