4c8497a37f6351eaa98f2060e49b114d9141256f
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-24 15:38:00 +0530

    Addition of the loading and error setting states and display approprite messgaes to the user. creating async await functions in useEffect to overcome the issue of creating the main returning function of useEffect which cannot be async

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,11 +6,17 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = (props) => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-a76d4-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const responseData = await response.json();
       console.log(responseData);
       const loadedMeals = [];
@@ -24,10 +30,29 @@ const AvailableMeals = (props) => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading ...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
   const mealList = meals.map((meal) => (
     <MealItem
       id={meal.id}
